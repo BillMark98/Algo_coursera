376fe5886b65c754b8fb185f36b5f273e8243745
BillHu <bill@136-197.eduroam.rwth-aachen.de>
2019-08-11 15:58:33 +0200

    week1 interview

---
+++ week1/MyUnionUF.java
@@ -0,0 +1,89 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class MyUnionUF {
+    private int[] id;
+    private int[] sz;
+    private int count;
+    // saves the largest element to which the element is connected
+    private int[] largestElement;
+
+    public MyUnionUF(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            sz[i] = i;
+        }
+        largestElement = new int[N];
+        for (int i = 0; i < N; i++) {
+            largestElement[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    // find the largest element to which p is connected
+    public int findLarge(int p) {
+        return largestElement[find(p)];
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+            if (largestElement[i] > largestElement[j]) {
+                largestElement[j] = largestElement[i];
+            }
+        }
+        else {
+            id[j] = i;
+            sz[i] += sz[j];
+            if (largestElement[j] > largestElement[i]) {
+                largestElement[i] = largestElement[j];
+            }
+        }
+
+        count--;
+    }
+
+    public static void main(String[] args) {
+        MyUnionUF myU = new MyUnionUF(10);
+        myU.union(1, 3);
+        myU.union(2, 4);
+        StdOut.println("Connected 1,3? : " + myU.connected(1, 3));
+        StdOut.println("Connected 2, 1 ? : " + myU.connected(1, 2));
+        StdOut.println("Largest connected to 1? " + myU.findLarge(1));
+        myU.union(6, 1);
+        myU.union(8, 2);
+        myU.union(9, 4);
+        myU.union(4, 3);
+        StdOut.println("Connected 6,8?: " + myU.connected(6, 8));
+        StdOut.println("Largest connected to 3? " + myU.findLarge(3));
+    }
+}
---
+++ week1/setDelSuccessor.java
@@ -0,0 +1,159 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class setDelSuccessor {
+    private MyUnionUF[] unionArr;
+    private int[] set;
+    private static final int deleted = -1;
+    private static final int noDelNeighbour = -1;
+    private static final int noSuccessor = -1;
+
+    public setDelSuccessor(int n) {
+        set = new int[n];
+        for (int i = 0; i < n; i++) {
+            set[i] = i;
+        }
+        unionArr = new MyUnionUF[n];
+    }
+
+    public void delete(int x) {
+        delNeighour delN = deletedNeighbor(x);
+        if (delN.neigh1 != noDelNeighbour) {
+            unionArr[delN.neigh1].union(delN.neigh1, x);
+            unionArr[x] = unionArr[delN.neigh1];
+            if (delN.doubleDeleted) {
+                unionArr[delN.neigh1].union(x, delN.neigh2);
+                unionArr[delN.neigh2] = unionArr[delN.neigh1];
+            }
+        }
+        else {
+            unionArr[x] = new MyUnionUF(set.length);
+        }
+        set[x] = deleted;
+    }
+
+    public int successor(int x) {
+        if (isInSet(x)) {
+            return x;
+        }
+        else {
+            int temp = unionArr[x].findLarge(x) + 1;
+            if (temp < set.length) {
+                return temp;
+            }
+            else {
+                StdOut.println("There are no such successor");
+                return noSuccessor;
+            }
+        }
+    }
+
+    public void printSet() {
+        for (int i : set) {
+            if (i != deleted)
+                StdOut.print(i + " ");
+        }
+        StdOut.println();
+    }
+
+    public void printSuccessor() {
+        for (int i = 0; i < set.length; i++) {
+            StdOut.println(i + " : " + successor(i));
+        }
+    }
+
+    private boolean isInSet(int x) {
+        return set[x] != deleted;
+    }
+
+    private delNeighour deletedNeighbor(int x) {
+        if (x == 0) {
+            if (set[1] == deleted) {
+                return new delNeighour(1);
+            }
+            else {
+                return new delNeighour(noDelNeighbour);
+            }
+        }
+        else if (x == set.length - 1) {
+            if (set[x - 1] == deleted) {
+                return new delNeighour(x - 1);
+            }
+            else {
+                return new delNeighour(noDelNeighbour);
+            }
+        }
+        else {
+            if (set[x - 1] == deleted) {
+                if (set[x + 1] == deleted) {
+                    return new delNeighour(x - 1, x + 1);
+                }
+                else {
+                    return new delNeighour(x - 1);
+                }
+            }
+            else if (set[x + 1] == deleted) {
+                return new delNeighour(x + 1);
+            }
+            else {
+                return new delNeighour(noDelNeighbour);
+            }
+        }
+    }
+
+    private class delNeighour {
+        private int neigh1, neigh2;
+        // whether the two neighbors(if they both exist) are deleted
+        private boolean doubleDeleted;
+
+        public delNeighour(int n1) {
+            neigh1 = n1;
+            doubleDeleted = false;
+        }
+
+        public delNeighour(int n1, int n2) {
+            neigh1 = n1;
+            neigh2 = n2;
+            doubleDeleted = true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        setDelSuccessor set = new setDelSuccessor(15);
+        StdOut.println("The set in the beginning");
+        set.printSet();
+        set.delete(4);
+        set.delete(7);
+        StdOut.println("now the set is: ");
+        set.printSet();
+        StdOut.println("successor print:");
+        set.printSuccessor();
+
+        set.delete(5);
+        set.delete(2);
+        StdOut.println("now the set is: ");
+        set.printSet();
+        StdOut.println("successor print:");
+        set.printSuccessor();
+
+        set.delete(6);
+        StdOut.println("now the set is: ");
+        set.printSet();
+        StdOut.println("successor print:");
+        set.printSuccessor();
+
+        set.delete(10);
+        set.delete(9);
+        set.delete(12);
+        StdOut.println("now the set is: ");
+        set.printSet();
+        StdOut.println("successor print:");
+        set.printSuccessor();
+    }
+}
